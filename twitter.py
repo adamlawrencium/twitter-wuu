3aa248f2ae7a9ff5115a45d9ddcacc1e1ad83fd6
adamadlawrence <adam.ad.lawrence@gmail.com>
2017-10-13 19:23:26 -0400

    doc and clean up

--- twitter.py
+++ twitter.py
@@ -14,50 +14,8 @@ for each other site
 
 import threading
 import time
-import Queue
 import sys
-
-q = Queue.Queue()
-
 import asyncore, socket
-
-# class Server(asyncore.dispatcher):
-#     def __init__(self, host, port):
-#         asyncore.dispatcher.__init__(self)
-#         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-#         self.bind(('', port))
-#         self.listen(1)
-
-#     def handle_accept(self):
-#         # when we get a client connection start a dispatcher for that
-#         # client
-#         socket, address = self.accept()
-#         print 'Connection by', address
-#         EchoHandler(socket)
-
-# class EchoHandler(asyncore.dispatcher_with_send):
-#     # dispatcher_with_send extends the basic dispatcher to have an output
-#     # buffer that it writes whenever there's content
-#     def handle_read(self):
-#         self.out_buffer = self.recv(1024)
-#         if not self.out_buffer:
-#             self.close()
-
-
-# class Client(asyncore.dispatcher_with_send):
-#     def __init__(self, host, port, message):
-#         asyncore.dispatcher.__init__(self)
-#         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-#         self.connect((host, port))
-#         self.out_buffer = message
-
-#     def handle_close(self):
-#         self.close()
-
-#     def handle_read(self):
-#         print 'Received', self.recv(1024)
-#         self.close()
-
 
 class Client(asyncore.dispatcher_with_send):
     def __init__(self, host, port, message):
@@ -75,7 +33,6 @@ class Client(asyncore.dispatcher_with_send):
 
 
 class EchoHandler(asyncore.dispatcher_with_send):
-
 	def handle_read(self):
 		data = self.recv(8192)
 		print "Messaged received"
@@ -83,7 +40,6 @@ class EchoHandler(asyncore.dispatcher_with_send):
 			self.send(data)
 
 class Server(asyncore.dispatcher):
-
 	def __init__(self, host, port):
 		asyncore.dispatcher.__init__(self)
 		self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -100,22 +56,21 @@ class Server(asyncore.dispatcher):
 			handler = EchoHandler(sock)
 
 
-
+# Main thread wrapper class
 class myThread (threading.Thread):
 	def __init__(self, name):
 		threading.Thread.__init__(self)
 		self.name = name
 		self.peers = [int(line.rstrip('\n')) for line in open('peers.txt')]
-
    
 	def run(self):
-		
+		# Enter while loop accepting the following commands
 		if self.name == 'commandThread':
 			while 1:
 				time.sleep(1)
 				command = raw_input("Please enter a command: ")
 				if command == "tweet":
-					self.tweetToAll('cool message')
+					self.tweetToAll('cool message') # testing for now
 				elif command == "view":
 					print self.peers
 				elif command == "quit":
@@ -125,10 +80,8 @@ class myThread (threading.Thread):
 				else:
 					print "Unknown command :(. Try again."
 		
-		
+		# Start the server the listening for incoming connections
 		elif self.name == 'serverThread':
-			# Read commands from Queue
-			# listens for incoming connections from peers
 			try:
 				server = Server('localhost', int(sys.argv[1]))
 				asyncore.loop()
@@ -136,18 +89,14 @@ class myThread (threading.Thread):
 				print "Cannot create server."
 				print sys.exc_info()[0]
 
+
+	# Connect to all peers send them <msg>
 	def tweetToAll(self, msg):
 		print "connecting to", self.peers
-		# connectedPeers = []
-		for peerPort in self.peers:
+		for peerPort in self.peers: # avoid connecting to self
 			if not peerPort != int(sys.argv[1]):
 				print "### Sending", msg, "to", peerPort
-				c = Client('', peerPort, "Hello friend")
-			
-			# connectedPeers.append(c)
-
-		# asyncore.loop()
-
+				c = Client('', peerPort, msg) # send <msg> to localhost at port <peerPort>
 
 if __name__ == "__main__":
 	
@@ -155,8 +104,6 @@ if __name__ == "__main__":
 	# Create new threads
 	commandThread = myThread("commandThread") # takes in raw_inputs and sends tweets to peers
 	serverThread = myThread("serverThread") # handles incoming connections from peers
-	# commandThread.daemon = True
-	# serverThread.daemon = True
 
 	# # Start new Threads
 	commandThread.start()
